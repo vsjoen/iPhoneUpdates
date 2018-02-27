c62d3b20b35c489a5ac054adbef647c3c4f84b81
Vegard Sjoen <vsjoen@gmail.com>
2018-02-27 06:50:37 +0100

    Added dynamic model list

--- iPhoneUpdates.py
+++ iPhoneUpdates.py
@@ -4,35 +4,101 @@ import fileinput
 import os.path
 import glob
 import time
-from colorama import init, Fore, Back, Style
+import re
 
-init()
 aList = []
-models = [
-            'iphone1,1',   # iPhone 2G
-            'iphone2,1',   # iPhone 3G
-            'iphone3,1',   # iPhone 4
-            'iphone4,1',   # iPhone 4s
-            'iphone5,1',   # iPhone 5
-            'iphone5,3',   # iPhone 5c
-            'iphone6,1',   # iPhone 5s
-            'iphone7,1',   # iPhone 6+
-            'iphone7,2',   # iPhone 6
-            'iphone8,1',   # iPhone 6s
-            'iphone8,2',   # iPhone 6s+
-            'iPhone8,4',   # iPhone SE
-            'iphone9,1',   # iPhone 7
-            'iphone9,2',   # iPhone 7+
-            'iphone10,4',  # iPhone 8
-            'iphone10,5',  # iPhone 8+
-            'iphone10,6'   # iPhone X
-        ]
+models = []
+
+file = open("models", "a+")
+for line in file:
+    models.append(line.splitlines()[0].split(","))
+
+
+# Find new GSM iPhone models and save to file
+def findGSM(a, b):
+    print "\n+------------------------------------------------------+"
+    print "|                                                      |"
+    print "|    Checking for new iPhone models, please wait...    |"
+    print "|                                                      |"
+    print "+------------------------------------------------------+\n"
+
+    kill = 0
+    i = int(a)
+    start = int(b)+1
+    while kill < 10:
+        for j in range(start, 11):
+
+            r = requests.get(
+                "http://api.ipsw.me/v2.1/iphone" +
+                str(i) + "," + str(j) + "/latest/url"
+                )
+
+            rN = requests.get(
+                "http://api.ipsw.me/v2.1/iphone" +
+                str(i) + "," + str(j) + "/latest/name"
+                )
+
+            Global = re.search("Global", rN.text)
+            CDMA = re.search("CDMA", rN.text)
+
+            if (str(r) == "<Response [200]>") and not Global and not CDMA:
+                print "  Added new model: " + rN.text
+                file.write(str(i) + "," + str(j) + "\n")
+                kill = 0
+
+            else:
+                kill += 1
+
+            start = 1
+
+        i += 1
+
+    file.close()
+    print "\n  No more models to add"
+
+
+# For every model, check for latest firmware
+def check():
+    print "\n+------------------------------------------------------+"
+    print "|                                                      |"
+    print "|        Downloading new firmware, please wait...      |"
+    print "|                                                      |"
+    print "+------------------------------------------------------+\n"
+
+    file = open("models", "r")
+    for line in file:
+        mod = line.splitlines()[0]
+        url = 'http://api.ipsw.me/v2.1/iphone' + mod + '/latest/url'
+        sizeURL = 'http://api.ipsw.me/v2.1/iphone' + mod + '/latest/filesize'
+        r = requests.get(url)
+        sizeR = requests.get(sizeURL)
+        file_name = r.text.split('/')[-1]
+        file_size = sizeR.text
+        link = r.text
+        delete_line = ''
+        aList.append(file_name)
+
+        # If no firmware exists for model, download new
+        if (os.path.isfile(file_name) is False):
+            downloadFile(link, file_name)
+
+        elif (file_size != str(os.stat(file_name).st_size)):
+            downloadFile(link, file_name)
+
+    # Filelist is the list of files in folder.
+    # Newlist is a list of the newest updates.
+    # Havelist is a list of files that are downloaded.
+    filelist = glob.glob("*.ipsw")
+    havelist = set(aList) & set(filelist)
+
+    print "\n  All firmware files are up to date"
+    deleteOld(filelist, havelist)
 
 
 # Download latest firmware
-def downloadFile():
-    with open(file_name, "wb") as f:
-        print "Downloading %s" % file_name
+def downloadFile(link, filename):
+    with open(filename, "wb") as f:
+        print "  Downloading %s" % filename
         response = requests.get(link, stream=True)
         total_length = response.headers.get('content-length')
 
@@ -47,43 +113,33 @@ def downloadFile():
             dl += len(data)
             f.write(data)
             done = int(50 * dl / total_length)
-            sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
+            sys.stdout.write("\r  [%s%s]" % ('=' * done, ' ' * (50-done)))
             sys.stdout.flush()
 
-# For every model, check for latest firmware
-for elem in reversed(models):
-    url = 'http://api.ipsw.me/v2.1/' + elem + '/latest/url'
-    sizeURL = 'http://api.ipsw.me/v2.1/' + elem + '/latest/filesize'
-    r = requests.get(url)
-    sizeR = requests.get(sizeURL)
-    file_name = r.text.split('/')[-1]
-    file_size = sizeR.text
-    link = r.text
-    delete_line = ''
-    aList.append(file_name)
+    print "\n"
 
-    # If no firmware exists for model, download new
-    if (os.path.isfile(file_name) is False):
-        downloadFile()
-
-    elif (file_size != str(os.stat(file_name).st_size)):
-        downloadFile()
-
-    # Else, all is well with the cosmos
-    else:
-        print (Fore.GREEN + Style.BRIGHT + file_name + " already exists.")
-
-# Filelist is the list of files in folder.
-# Newlist is a list of the newest updates.
-# Havelist is a list of files that are downloaded.
-filelist = glob.glob("*.ipsw")
-havelist = set(aList) & set(filelist)
 
 # Delete all files not in havelist
-for f in filelist:
-    if f not in havelist:
-        print (Fore.RED + Style.BRIGHT + f + " has been deleted")
-        os.remove(f)
+def deleteOld(filelist, havelist):
+    print "\n+------------------------------------------------------+"
+    print "|                                                      |"
+    print "|              Deleting old firmware files             |"
+    print "|                                                      |"
+    print "+------------------------------------------------------+\n"
 
-print (Style.RESET_ALL + "\n" + "Closing this window in 10 seconds.")
+    for f in filelist:
+        if f not in havelist:
+            print ("  " + f + " deleted")
+            os.remove(f)
+
+    print "\n  Old firmware files deleted\n"
+
+
+if not models:
+    findGSM(3, 0)
+
+else:
+    findGSM(*models[-1])
+
+check()
 time.sleep(10)
